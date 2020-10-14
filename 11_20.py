ea9b970676cb938ab17607a0d85f3cffbd5514c6
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-14 17:44:31 +0200

    Update 11_20.py
    + aggiunto es 11

--- 11_20.py
+++ 11_20.py
@@ -4,6 +4,10 @@ In Canada, fuel efficiency is normally expressed in liters-per-hundred
 kilometers (L/100 km). Use your research skills to determine how to convert from
 MPG to L/100 km. Then create a program that reads a value from the user in American
 units and displays the equivalent fuel efficiency in Canadian units. """
+miglia_per_gallone = 282.48
+benzina_consumata = int(input("Inserisci la quantità di benzina consumata: "))
+litri_per_100kilometri = miglia_per_gallone / benzina_consumata
+print("Conversione di carburante effettuata: ", litri_per_100kilometri)
 
 # ESERCIZIO 12
 """ The surface of the Earth is curved, and the distance between degrees of longitude
