5041391b7d37e18949849b048828e849a66a881a
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-14 17:22:34 +0200

    Update 1_10.py
    + modulo math

--- 1_10.py
+++ 1_10.py
@@ -1,3 +1,5 @@
+from math import log10
+
 # ESERCIZIO 1
 """ Create a program that displays your name and complete mailing address formatted in
 the manner that you would usually see it on the outside of an envelope. Your program
