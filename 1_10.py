523410546ce910b4b52ce1b970d8b63ec25d5116
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-14 17:07:26 +0200

    Update 1_10.py

--- 1_10.py
+++ 1_10.py
@@ -79,6 +79,13 @@ print("La somma di", n, "agli interi positivi è:", somma)
 grams. Each gizmo weighs 112 grams. Write a program that reads the number of
 widgets and the number of gizmos in an order from the user. Then your program
 should compute and display the total weight of the order. """
+peso_widget = 75 # grammi
+peso_gizmo = 112 # grammi
+widget = int(input("Inserisci il numero di Widget: "))
+gizmo = int(input("Inserisci il numero di Gizmo: "))
+tot_widget = widget * peso_widget
+tot_gizmo = gizmo * peso_gizmo
+print("Peso totale ordine:", tot_gizmo + tot_widget, "grammi")
 
 # ESERCIZIO 9
 """ Pretend that you have just opened a new savings account that earns 4 percent interest
