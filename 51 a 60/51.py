88630b1ef8bc1e0a6cbed05101ec4bd1b1c9f1cb
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-15 23:21:04 +0200

    Create 51.py

---
+++ 51 a 60/51.py
@@ -0,0 +1,4 @@
+""" At a particular university, letter grades are mapped to grade points in the following
+manner: Write a program that begins by reading a letter grade from the user. Then your
+program should compute and display the equivalent number of grade points. Ensure that your program
+generates an appropriate error message if the user enters an invalid letter grade. """
