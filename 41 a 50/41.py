e595222b985a8bbc0f774d636fef5bc86d768f54
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-15 15:46:54 +0200

    Create 41.py

---
+++ 41 a 50/41.py
@@ -0,0 +1,11 @@
+""" Begin by writing a program that reads the name of a note from the user and
+displays the note’s frequency. Your program should support all of the notes listed
+previously.
+Once you have your program working correctly for the notes listed previously you
+should add support for all of the notes from C0 to C8. While this could be done by
+adding many additional cases to your if statement, such a solution is cumbersome,
+inelegant and unacceptable for the purposes of this exercise. Instead, you should
+exploit the relationship between notes in adjacent octaves. In particular, the frequency
+of any note in octave n is half the frequency of the corresponding note in octave n+1.
+By using this relationship, you should be able to add support for the additional notes
+without adding additional cases to your if statement. """
