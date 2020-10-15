dedbaf2797b66b9721df4a99399803ca219b627d
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-15 11:23:47 +0200

    Update 16.py

--- 11 a 20/16.py
+++ 11 a 20/16.py
@@ -1,3 +1,4 @@
+import math
 """ Write a program that begins by reading a radius, r, from the user. The program will
 continue by computing and displaying the area of a circle with radius r and the
 volume of a sphere with radius r. Use the pi constant in the math module in your
