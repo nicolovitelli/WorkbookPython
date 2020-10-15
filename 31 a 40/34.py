286091994336fb2bbded16e4dc4a26552b7497e4
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-15 14:45:40 +0200

    Create 34.py

---
+++ 31 a 40/34.py
@@ -0,0 +1,7 @@
+""" Write a program that reads an integer from the user. Then your program should
+display a message indicating whether the integer is even or odd."""
+numero = int(input("Inserisci numero: "))
+if numero % 2 == 0:
+    print("Numero Pari")
+else:
+    print("Numero Dispari")
