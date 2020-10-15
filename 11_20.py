7a0a6986c990786423ff88617a8f3b8d7edcded5
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-15 10:26:37 +0200

    Update 11_20.py
    + aggiunto es 16

--- 11_20.py
+++ 11_20.py
@@ -93,6 +93,11 @@ print("Misura in Miglia:", miglia)
 continue by computing and displaying the area of a circle with radius r and the
 volume of a sphere with radius r. Use the pi constant in the math module in your
 calculations. """
+raggio = int(input("Inserisci il Raggio: "))
+area = math.pi * (raggio ^ 2)
+volume = 4/3 * math.pi * (raggio ^ 3)
+print("L'Area del Cerchio è:", area)
+print("Il Volume della Sfera è:", volume)
 
 # ESERCIZIO 17
 """ The amount of energy required to increase the temperature of one gram of a material
