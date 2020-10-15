959dff538d6a062715953cdc5b875c50003929fa
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-15 10:42:54 +0200

    Update 11_20.py
    + aggiunto es 19

--- 11_20.py
+++ 11_20.py
@@ -136,6 +136,10 @@ ground. The user will enter the height from which the object is dropped in meter
 Because the object is dropped its initial speed is 0 m/s. Assume that the acceleration
 due to gravity is 9.8 m/s2. You can use the formula vf = v2 i + 2ad to compute the
 final speed, vf , when the initial speed, vi , acceleration, a, and distance, d, are known. """
+altezza = int(input("Inserisci l'Altezza (in metri) dalla quale l'oggetto cade: "))
+gravita = 9.8
+vf = sqrt(2 * gravita * altezza)
+print("Colpirà il terreno a %.2f m/s" % vf)
 
 # ESERCIZIO 20
 """ The ideal gas law is a mathematical approximation of the behavior of gasses as
