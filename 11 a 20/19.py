dd43de6fb33b69a248962d180bd5d5ceb9527d7b
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-15 11:23:09 +0200

    Update 19.py

--- 11 a 20/19.py
+++ 11 a 20/19.py
@@ -1,3 +1,4 @@
+import math
 """ Create a program that determines how quickly an object is traveling when it hits the
 ground. The user will enter the height from which the object is dropped in meters (m).
 Because the object is dropped its initial speed is 0 m/s. Assume that the acceleration
