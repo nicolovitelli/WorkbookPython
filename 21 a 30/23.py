a4ebc600008c39c8fba8be517ffd142819a1f760
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-15 11:22:33 +0200

    Update 23.py

--- 21 a 30/23.py
+++ 21 a 30/23.py
@@ -1,3 +1,4 @@
+import math
 """ A polygon is regular if its sides are all the same length and the angles between all of
 the adjacent sides are equal. The area of a regular polygon can be computed using
 the following formula, where s is the length of a side and n is the number of sides: area = n × s2 / 4 × tan π n
