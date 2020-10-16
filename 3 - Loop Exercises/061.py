a3b52061a1587c36db0deecb79243411ca4e31df
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-16 22:34:12 +0200

    Update 061.py

--- 3 - Loop Exercises/061.py
+++ 3 - Loop Exercises/061.py
@@ -2,3 +2,13 @@
 of values entered by the user. The user will enter 0 as a sentinel value to indicate
 that no further values will be provided. Your program should display an appropriate
 error message if the first value entered by the user is 0. """
+somma = 0.0
+numero = 1
+valore = 0
+while (numero != 0):
+    valore = int(input("Valore: "))
+    somma = somma + valore
+    numero = valore
+if numero == 0:
+    print("Fine")
+    print("Somma", somma)
