3e9af68c9b1c2398ba905f1fa23fa67a2ee4a58f
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-17 15:53:12 +0200

    Create 074.py

---
+++ 3 - Loop Exercises/074.py
@@ -0,0 +1,22 @@
+""" In this exercise you will create a program that displays a multiplication table that
+shows the products of all combinations of integers from 1 times 1 up to and including
+10 times 10. Your multiplication table should include a row of labels across the top
+of it containing the numbers 1 through 10. It should also include labels down the left
+side consisting of the numbers 1 through 10. The expected output from the program
+is shown below: When completing this exercise you will probably find it helpful to be able to
+print out a value without moving down to the next line. This can be accomplished
+by added end="" as the last parameter to your print statement. For example,
+print("A") will display the letter A and then move down to the next line. The
+statement print("A", end="") will display the letter A without moving down
+to the next line, causing the next print statement to display its result on the same line as the letter A."""
+minimo = 1
+massimo = 10
+print("    ", end="")
+for i in range(minimo, massimo + 1):
+    print("%4d" % i, end="")
+print()
+for i in range(minimo, massimo + 1):
+    print("%4d" % i, end="")
+    for j in range(minimo, massimo + 1):
+        print("%4d" % (i * j), end="")
+    print()
