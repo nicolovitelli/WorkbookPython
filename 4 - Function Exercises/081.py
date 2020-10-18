9b0c76e99387e79b2eb2db41f70eab6a60015c61
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-18 10:44:46 +0200

    Update 081.py

--- 4 - Function Exercises/081.py
+++ 4 - Function Exercises/081.py
@@ -3,3 +3,10 @@ its parameters. Return the hypotenuse of the triangle, computed using Pythagorea
 theorem, as the function’s result. Include a main program that reads the lengths of
 the shorter sides of a right triangle from the user, uses your function to compute the
 length of the hypotenuse, and displays the result. """
+def ipotenusa_triangolo(lato1, lato2):
+    ipotenusa = math.sqrt((lato1 ** 2) + (lato2 ** 2))
+    print("L'Ipotenusa è uguale a:", round(ipotenusa,2))
+
+lato1 = float(input("Inserisci lunghezza Lato 1: "))
+lato2 = float(input("Inserisci Lunghezza Lato 2: "))
+ipotenusa_triangolo(lato1, lato2)
