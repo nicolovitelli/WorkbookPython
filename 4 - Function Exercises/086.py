afb64ecb94fce7bfc39ac40190a60350b624a14d
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-18 12:08:22 +0200

    Update 086.py

--- 4 - Function Exercises/086.py
+++ 4 - Function Exercises/086.py
@@ -69,12 +69,12 @@ def dodici_giorni_natale(verso):
         print(verso_12, "\n")
 
 def main():
-verso = int(input("Inserisci Verso da Visualizzare: "))
-dodici_giorni_natale(verso)
-valore = 1
-while valore != 12:
-    dodici_giorni_natale(valore)
-    valore = valore + 1
+    verso = int(input("Inserisci Verso da Visualizzare: "))
+    dodici_giorni_natale(verso)
+    valore = 1
+    while valore != 12:
+        dodici_giorni_natale(valore)
+        valore = valore + 1
 
 if __name__ == '__main__':
     main()
