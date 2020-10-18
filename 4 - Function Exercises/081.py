bd583590069c7da84ef51e2a16fbdbff0c1c2c31
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-18 11:24:26 +0200

    Update 081.py

--- 4 - Function Exercises/081.py
+++ 4 - Function Exercises/081.py
@@ -1,3 +1,5 @@
+import math
+
 """ Write a function that takes the lengths of the two shorter sides of a right triangle as
 its parameters. Return the hypotenuse of the triangle, computed using Pythagorean
 theorem, as the function’s result. Include a main program that reads the lengths of
