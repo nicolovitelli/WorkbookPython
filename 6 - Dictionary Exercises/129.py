f599029517d65d05c58700ee2803c3d36d24ab50
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-22 11:03:57 +0200

    Create 129.py

---
+++ 6 - Dictionary Exercises/129.py
@@ -0,0 +1,24 @@
+""" In this exercise you will simulate 1,000 rolls of two dice. Begin by writing a function
+that simulates rolling a pair of six-sided dice. Your function will not take any
+parameters. It will return the total that was rolled on two dice as its only result.
+Write a main program that uses your function to simulate rolling two six-sided
+dice 1,000 times. As your program runs, it should count the number of times that each
+total occurs. Then it should display a table that summarizes this data. Express the
+frequency for each total as a percentage of the total number of rolls. Your program
+should also display the percentage expected by probability theory for each total.
+Sample output is shown below. """
+def lancioDadi():
+    minimo = 1
+    massimo = 6
+    primo_dado = random.randint(minimo, massimo)
+    secondo_dado = random.randint(minimo, massimo)
+    print("Primo Dado:", primo_dado, end="")
+    print(" | Secondo Dado:", secondo_dado)
+
+def main():
+    for i in range(1000):
+        lancioDadi()
+
+main()
+
+# analisi dei dati mancanti
