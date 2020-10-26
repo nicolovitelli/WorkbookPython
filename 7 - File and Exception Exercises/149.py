c25636465c5110b6a862ce0104fc087e4b87fcf5
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-26 11:41:17 +0100

    Create 149.py

---
+++ 7 - File and Exception Exercises/149.py
@@ -0,0 +1,8 @@
+""" Write a program that converts from letter grades to grade points and vice-versa.
+Your program will convert multiple values entered by the user, with one value
+entered on each line. Begin by attempting to convert each value entered by the
+user from a number of grade points to a letter grade. If an exception occurs during
+the attempt then your program should attempt to convert the value from a letter grade to a number of grade points.
+If both conversions fail then your program should provide a message indicating that the supplied input is invalid.
+Design your program so that it continues performing conversions until the user enters a blank
+line. Your solutions to Exercises 51 and 52 may be helpful when completing this exercise. """
