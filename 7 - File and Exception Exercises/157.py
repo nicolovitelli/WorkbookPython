37dfffd83c2b0217aa30b2f38fd6db204beb7297
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-27 12:33:22 +0100

    Create 157.py

---
+++ 7 - File and Exception Exercises/157.py
@@ -0,0 +1,6 @@
+""" In this exercise, you will create a program that reads every file in the baby names data
+set described in Exercise 154. As your program reads the files, it should keep track
+of each name used for a boy and each name used for a girl. Your program should
+output two lists. One list will contain all of the names that have been used for girls.
+The other list will contain all of the names that have been used for boys. Neither of
+your lists should contain any repeated values. """
