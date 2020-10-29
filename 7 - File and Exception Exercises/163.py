50e912ae436e45ec0f4b36724865f6c564271b21
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-29 10:55:05 +0100

    Create 163.py

---
+++ 7 - File and Exception Exercises/163.py
@@ -0,0 +1,6 @@
+""" There is at least one word in the English language that contains each of the vowels
+a, e, i, o, u and y exactly once and in order. Write a program that searches a file
+containing a list of words and displays all of the words that meet this constraint. The
+user will provide the name of the file that will be searched. Display an appropriate
+error message and exit the program if the user provides an invalid file name or if
+something else goes wrong while searching for words with six vowels in order. """
