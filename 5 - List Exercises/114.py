e40a138d37f6ddc149884bbf0b8cd36673088a34
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-10-30 11:10:07 +0100

    + import random

--- 5 - List Exercises/114.py
+++ 5 - List Exercises/114.py
@@ -1,3 +1,5 @@
+import random
+
 """ In order to win the top prize in a particular lottery, one must match all 6 numbers
 on his or her ticket to the 6 numbers between 1 and 49 that are drawn by the lottery
 organizer. Write a program that generates a random selection of 6 numbers for a
