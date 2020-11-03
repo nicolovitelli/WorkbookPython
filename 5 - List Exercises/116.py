b9370787816ebe101d7edb6a30b4adc0a3f0c4de
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-11-03 11:46:46 +0100

    Update 116.py

--- 5 - List Exercises/116.py
+++ 5 - List Exercises/116.py
@@ -6,3 +6,23 @@ the word should be changed to lowercase. For example, Computer should become
 Omputercay. If a word ends in a punctuation mark then the punctuation mark
 should remain at the end of the word after the transformation has been performed.
 For example, Science! should become Iencescay!. """
+ay = "ay"
+way = "way"
+consonante = ("B","C", "D","F","G","H","J","K","L","M","N","P","Q","R","S","T","Y","V","X","Z")
+vowel = ("A","E","I","O","U")
+parola_utente = input("Inserisci una parola da tradurre: ")
+prima_lettera = parola_utente[0]
+prima_lettera = str(prima_lettera)
+prima_lettera=prima_lettera.upper()
+if prima_lettera in consonante:
+    print(prima_lettera,"è una consonante")
+    lunghezza_parola = len(parola_utente)
+    rimuovi_prima_lettera = parola_utente[1:lunghezza_parola]
+    pig_latin=rimuovi_prima_lettera+prima_lettera+ay
+    print("La parola in Pig Latin è:",pig_latin)
+elif prima_lettera in vowel:
+    print(prima_lettera,"is a vowel")
+    pig_latin=parola_utente+way
+    print("La parola in Pig Latin è:",pig_latin)
+else:
+    print("Non so cosa",prima_lettera,"sia")
