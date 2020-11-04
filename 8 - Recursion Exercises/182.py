70177b20b3a532be45bec927f47c4122d304f819
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-11-04 17:29:26 +0100

    Create 182.py

---
+++ 8 - Recursion Exercises/182.py
@@ -0,0 +1,4 @@
+""" Write a recursive function that determines whether or not a word can be spelled using only element symbols.
+ Your function will require two parameters: the word that are you trying to spell and a list of the symbols that can
+ be used. It will return a string containing the symbols used to achieve the spelling as its result, or an empty string
+ if no spelling exists. """
